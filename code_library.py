7c7327a6d57acb9715545a7901813ddcf9434fe2
Alan Xie <alanxieyulong@gmail.com>
2021-07-15 15:58:38 +0800

    Add comment

--- code_library.py
+++ code_library.py
@@ -12,7 +12,9 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        # read database from csv on shared drive
         self.db = pd.read_csv("S:/AlanXie/code.csv")
+        # calculate number of levels
         self.LEVEL = self.db.shape[1] - (len(self.db.columns) - [i for i, word in enumerate(self.db.columns) if word.startswith('Unnamed:')][0])
 
         self.setWindowTitle("Code Library")
@@ -23,6 +25,7 @@ class MainWindow(QMainWindow):
         main_layout = QHBoxLayout()
         self.stacklayout = QStackedLayout()
 
+        # create des_layout on the right side
         des_label_title = QLabel("Description:")
         self.des_label = QLabel("")
         self.des_label.setWordWrap(True)
@@ -33,14 +36,15 @@ class MainWindow(QMainWindow):
         des_layout.addWidget(self.des_label, 7)
         des_layout.addWidget(dir_label_title, 1)
         des_layout.addWidget(self.dir_label, 1)
-
         des_layout.setAlignment(Qt.AlignTop)
         self.des_label.setAlignment(Qt.AlignTop)
         self.dir_label.setAlignment(Qt.AlignTop)
 
+        # add main_layout on the left side
         main_layout.addLayout(self.stacklayout, 5)
         main_layout.addLayout(des_layout, 5)
 
+        # add run_layout at the buttom
         run_layout = QHBoxLayout()
         pagelayout.addLayout(menu_layout)
         pagelayout.addLayout(main_layout)
@@ -55,6 +59,7 @@ class MainWindow(QMainWindow):
         self.list[0].setup(sorted(set(self.db.iloc[:,0])))
         self.list[0].createMap(self.db.iloc[self.getCurrentSelectedRows()])
 
+        # add back button at the top left corner (in menu)
         self.back_btn = QPushButton("<")
         self.back_btn.setFixedWidth(50)
         self.back_btn.pressed.connect(self.goBack)
@@ -63,6 +68,7 @@ class MainWindow(QMainWindow):
         menu_layout.addWidget(self.back_btn)
         menu_layout.addWidget(self.path_label)
 
+        # add run button at the bottom
         self.run_btn = QPushButton("run")
         self.run_btn.setEnabled(False)
         self.run_btn.index = -1
@@ -70,11 +76,14 @@ class MainWindow(QMainWindow):
         run_layout.addWidget(self.run_btn)
 
         # Testing!!!
+        # uncomment the below lines and add test function in test()
+        # remember to also uncomment the test() function below
         self.test_btn = QPushButton("test")
         self.test_btn.pressed.connect(self.test)
         run_layout.addWidget(self.test_btn)
         # Testing!!!
 
+        # add empty list to list
         for i in self.list:
             self.stacklayout.addWidget(i)
 
@@ -82,28 +91,23 @@ class MainWindow(QMainWindow):
         widget.setLayout(pagelayout)
         self.setCentralWidget(widget)
 
+    # called when items in the list is clicked
     def listClick(self):
+        # get the current level
         idx = self.stacklayout.currentIndex()
-        # print("clicked list " + str(idx))
+        # if the last level is reached
+        # if you have more levels, it is recommended to add level columns in the csv
         if idx >= self.LEVEL - 1:
-            # print("ready to run")
             self.fileClick()
         else:
-            # print(self.list[0].currentItem().text())
             selected = self.list[idx].currentItem().text()
-            # print("length is " + str(len(self.db.iloc[self.list[idx].map(selected)])))
-            # print(str(self.db.iloc[self.list[idx].map(selected)]))
             if len(self.db.iloc[self.list[idx].map(selected)]) == 1:
                 row = self.db.iloc[self.list[idx].map(selected)].index
-                # print(row)
-                # print(idx)
-                # print(self.db.iloc[row, idx+1].values[0])
-                # print(type(self.db.iloc[row, idx+1].values[0]))
                 if (not isinstance(self.db.iloc[row, idx+1].values[0], str)) and math.isnan(self.db.iloc[row, idx+1]):
                     self.fileClick()
                     return
 
-            # print(self.db[self.db.iloc[:,0] == self.list[0].currentItem().text()].iloc[:,1].values)
+            # item clicked is not a file, enter the next level, set up the list for next level
             self.list[idx+1].clear()
             self.list[idx+1].setup(sorted(self.db.iloc[self.list[idx].map(selected)].iloc[:,idx+1].values))
             self.stacklayout.setCurrentIndex(idx+1)
@@ -113,8 +117,11 @@ class MainWindow(QMainWindow):
             self.run_btn.setEnabled(False)
             self.run_btn.index = -1
 
+        # update the path in the menu
         self.updatePath(idx)
 
+    # called when the item in the list clicked is a file
+    # update the description on the right and enable the run button at the bottom
     def fileClick(self):
         self.run_btn.setEnabled(True)
         idx = self.stacklayout.currentIndex()
@@ -131,6 +138,7 @@ class MainWindow(QMainWindow):
         except:
             self.des_label.setText("")
 
+    # called when the back button at the top left corner is clicked, going back to the previous level
     def goBack(self):
         self.des_label.setText("")
         self.dir_label.setText("")
@@ -145,28 +153,38 @@ class MainWindow(QMainWindow):
         else:
             self.updatePath(idx-2)
 
+    # update the path in the menu
     def updatePath(self, idx):
         path_list = []
         for i in range(idx+1):
             path_list.append(self.list[i].currentItem().text())
         self.path_label.setText(" > ".join(path_list))
 
+    # called when the run button in clicked, run the file
     def openFile(self):
         file = self.dir_label.text()
         extension = file.split(".")[-1]
+        # simply open the excel file
         if extension == "csv" or extension == "xlsm":
             os.system("start " + file)
+            print("open " + file)
             # MACOS: os.system("open " + filename)
             # Windows: os.system("start " + filename)
+        # run python script
         elif extension == "py":
+            print("run " + file)
+            # if input is required for the python file
             if isinstance(self.db['require_input'].values[self.run_btn.index], str):
                 input = self.db['require_input'].values[self.run_btn.index]
 
+                # if the input required is a form
                 if self.db['input_type'].values[self.run_btn.index] == "form":
                     dialog = MultiInputDialog(input.split(";"))
+                # if the input required is a list
                 elif self.db['input_type'].values[self.run_btn.index] == "list":
                     dialog = ListInputDialog(input)
 
+                # if default text is needed
                 if self.db['default'].values[self.run_btn.index] == 'Y':
                     spec = importlib.util.spec_from_file_location("module", file)
                     foo = importlib.util.module_from_spec(spec)
@@ -177,6 +195,8 @@ class MainWindow(QMainWindow):
                         print("Error on setup")
                         return
                     dialog.setDefault(default_list)
+
+                # pop up the input dialog and wait for user input
                 if dialog.exec():
                     retval = dialog.getInputs()
 
@@ -184,19 +204,23 @@ class MainWindow(QMainWindow):
                     foo = importlib.util.module_from_spec(spec)
                     spec.loader.exec_module(foo)
 
+                    # pop up the confirm dialog, python won't be executed if user chooses no
                     confirm_msg = foo.confirm(retval)
                     reply = QMessageBox.question(self, 'Confirm', confirm_msg, QMessageBox.Yes | QMessageBox.No)
                     if reply == QMessageBox.Yes:
                         try:
                             ret_msg = foo.function(retval)
                         except:
+                            # error message if the python fucntion is buggy
                             print("The source function is buggy.")
 
+                        # python file executed unsuccessfully
                         if ret_msg is not None and ret_msg["status"] == 0:
                             msg = QMessageBox()
                             msg.setText("Error: " + ret_msg["msg"])
                             msg.setWindowTitle("Fail")
                             retval = msg.exec_()
+                        # python file executed successfully
                         elif ret_msg is not None and ret_msg["status"] == 1:
                             msg = QMessageBox()
                             msg.setText(ret_msg["msg"])
@@ -207,19 +231,23 @@ class MainWindow(QMainWindow):
                                 if ret_msg["type"] == "dir":
                                     os.startfile(ret_msg["value"])
 
+            # run python that doesn't need input, python is required on the user's computer
+            # but no customization is reuiqred on the python scirpt
             else:
                 os.system("python " + file)
 
+    # get indices of items in the database which are under items in the currently list
     def getCurrentSelectedRows(self):
         idx = self.stacklayout.currentIndex()
         if idx <= 0:
             return range(len(self.db))
         else:
-            # print("selecting from list " + str(idx))
             prev_selected = self.list[idx-1].currentItem().text()
             return self.list[idx-1].map(prev_selected)
 
     # Testing!!!
+    # uncomment this and add your test function here
+    # rememeber to also uncomment the button creation codes above
     def test(self):
         dialog = ListInputDialog("RTL")
         if dialog.exec():
--- customized_widget.py
+++ customized_widget.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import *
 
+# list widget for the list in the main window
 class ListWidget(QListWidget):
     def __init__(self, idx):
         super().__init__()
@@ -9,16 +10,20 @@ class ListWidget(QListWidget):
         for i in set(list):
             self.addItem(i)
 
+    # create a map from the items in the current list to the sub-items under each of them
     def createMap(self, df):
         item_list = set(self.filter_nan(df.iloc[:, self.idx]))
         self.item2index = {i: df[df.iloc[:,self.idx] == i].index for i in item_list}
 
+    # return the sub-items of this item
     def map(self, current_item):
         return self.item2index[current_item]
 
+    # remove nan
     def filter_nan(self, df):
         return df[df.notna()]
 
+# input dialog for a form, which means different kinds of inputs are required
 class MultiInputDialog(QDialog):
     def __init__(self, question_list):
         super().__init__()
@@ -45,6 +50,74 @@ class MultiInputDialog(QDialog):
         for i in range(len(default_list)):
             self.line_edit_list[i].setText(default_list[i])
 
+# input dialog for a list, which means multiple values of a same category is required
+class ListInputDialog(QDialog):
+
+    def __init__(self, title):
+        super().__init__()
+
+        self.title = title
+
+        buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel, self);
+
+        main_layout = QFormLayout(self)
+
+        self.list_layout = QVBoxLayout()
+
+        header = QHBoxLayout()
+        title_label = QLabel(self.title)
+        header.addWidget(title_label)
+        add_btn = QPushButton("+")
+        add_btn.setFixedWidth(50)
+        add_btn.pressed.connect(self.addRow)
+        header.addWidget(add_btn)
+
+        main_layout.addRow(header)
+
+        main_layout.addRow(self.list_layout)
+
+        self.list_layout.addLayout(self.inputRow())
+
+        main_layout.addRow(buttonBox)
+
+        buttonBox.accepted.connect(self.accept)
+        buttonBox.rejected.connect(self.reject)
+
+    def getInputs(self):
+        return [self.list_layout.itemAt(i).getInput() for i in range(self.list_layout.count())]
+
+    def setDefault(self, default_list):
+        self.list_layout.itemAt(0).setInput(default_list[0])
+        for i in range(1, len(default_list)):
+            self.addRow()
+            self.list_layout.itemAt(i).setInput(default_list[i])
+
+    def addRow(self):
+        self.list_layout.addLayout(self.inputRow())
+
+    # Each inputRow is a layout with a input field on the left and a delete button on the right
+    class inputRow(QHBoxLayout):
+        def __init__(self):
+            super().__init__()
+
+            self.input_field = QLineEdit()
+            self.addWidget(self.input_field)
+            self.del_btn = QPushButton("x")
+            self.del_btn.setFixedWidth(50)
+            self.del_btn.pressed.connect(self.deleteThisRow)
+            self.addWidget(self.del_btn)
+
+        def deleteThisRow(self):
+            self.input_field.deleteLater()
+            self.del_btn.deleteLater()
+            self.deleteLater()
+            pass
+
+        def getInput(self):
+            return self.input_field.text()
+
+        def setInput(self, s):
+            self.input_field.setText(s)
 
 # class ListInputDialog(QDialog):
 #
@@ -109,70 +182,3 @@ class MultiInputDialog(QDialog):
 #                     self.line_edit_list.remove(self.line_edit_list[i])
 #                     self.button_list.remove(self.sender())
 #                     break
-
-class ListInputDialog(QDialog):
-
-    def __init__(self, title):
-        super().__init__()
-
-        self.title = title
-
-        buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel, self);
-
-        main_layout = QFormLayout(self)
-
-        self.list_layout = QVBoxLayout()
-
-        header = QHBoxLayout()
-        title_label = QLabel(self.title)
-        header.addWidget(title_label)
-        add_btn = QPushButton("+")
-        add_btn.setFixedWidth(50)
-        add_btn.pressed.connect(self.addRow)
-        header.addWidget(add_btn)
-
-        main_layout.addRow(header)
-
-        main_layout.addRow(self.list_layout)
-
-        self.list_layout.addLayout(self.inputRow())
-
-        main_layout.addRow(buttonBox)
-
-        buttonBox.accepted.connect(self.accept)
-        buttonBox.rejected.connect(self.reject)
-
-    def getInputs(self):
-        return [self.list_layout.itemAt(i).getInput() for i in range(self.list_layout.count())]
-
-    def setDefault(self, default_list):
-        self.list_layout.itemAt(0).setInput(default_list[0])
-        for i in range(1, len(default_list)):
-            self.addRow()
-            self.list_layout.itemAt(i).setInput(default_list[i])
-
-    def addRow(self):
-        self.list_layout.addLayout(self.inputRow())
-
-    class inputRow(QHBoxLayout):
-        def __init__(self):
-            super().__init__()
-
-            self.input_field = QLineEdit()
-            self.addWidget(self.input_field)
-            self.del_btn = QPushButton("x")
-            self.del_btn.setFixedWidth(50)
-            self.del_btn.pressed.connect(self.deleteThisRow)
-            self.addWidget(self.del_btn)
-
-        def deleteThisRow(self):
-            self.input_field.deleteLater()
-            self.del_btn.deleteLater()
-            self.deleteLater()
-            pass
-
-        def getInput(self):
-            return self.input_field.text()
-
-        def setInput(self, s):
-            self.input_field.setText(s)
