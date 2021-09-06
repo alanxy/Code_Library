bfc8aa5b9880a7efc72cabaa2c6aa0ea036c4769
Alan Xie <alanxieyulong@gmail.com>
2021-09-06 13:42:55 +0800

    auto set win console quick edit mode off to avoid unexpected gui pause

--- code_library.py
+++ code_library.py
@@ -11,8 +11,13 @@ from customized_widget import *
 import logging
 import getpass
 import datetime
+import ctypes
 
 logging.basicConfig(level=logging.INFO, filename = "C:/Users/" + getpass.getuser() + "/Downloads/code_library_logging_" + datetime.datetime.now().strftime("%Y%m%d_%H%M%S") + ".log")
+
+# disable winsows console quick edit mode ref: https://stackoverflow.com/questions/13599822/command-prompt-gets-stuck-and-continues-on-enter-key-press
+kernel32 = ctypes.windll.kernel32
+kernel32.SetConsoleMode(kernel32.GetStdHandle(-10), 128)
 
 class MainWindow(QMainWindow):
     def __init__(self):
